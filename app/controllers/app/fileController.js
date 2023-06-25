e0bc7a919083a2d0cdc09d47f51b79b016e677ff
aiyin <372020407@qq.com>
2023-06-25 20:11:40 +0800

    #update update docx/pdf

--- app/controllers/app/fileController.js
+++ app/controllers/app/fileController.js
@@ -79,7 +79,7 @@ exports.create =async (req, res) => {
             console.log(err.message || err)
             res.status(500).send({
                 message:
-                    "文件上传失败，请稍后再尝试"
+                    "暂不支持word2007及之前前的doc文件解析"
             });
         }
     }
